6edcbfdd5d976608ff29b70c7a97d49b6701f3df
JJ <jeongjae1996@naver.com>
2023-01-15 01:14:28 +0900

    [feat] 저장된 모델을 통해 inference할 수 있도록 기능 추가 #3

--- examples/baseline/template/config.py
+++ examples/baseline/template/config.py
@@ -1,10 +1,11 @@
 CFG = {
     'FPS':30,
-    'IMG_SIZE':128,
-    'EPOCHS':10,
+    'IMG_SIZE':160,
+    'EPOCHS':20,
     'LEARNING_RATE':3e-4,
     'BATCH_SIZE':4,
     'SEED':41,
     'VAL_RATIO': 0.2,
-    'WORKING_DIR': 'D:/Assignments/dacon/TVControlHandGestureRecognition/'
+    'WORKING_DIR': 'D:/Assignments/dacon/TVControlHandGestureRecognition/',
+    'WEIGHTS': 'best.pth' 
 }
--- examples/baseline/template/inference.py
+++ examples/baseline/template/inference.py
@@ -14,3 +14,35 @@ def inference(model, test_loader, device):
 
             preds += logit.argmax(1).detach().cpu().numpy().tolist()
     return preds
+
+if __name__ == "__main__":
+    import os
+    import pandas as pd
+
+    from data import load_data
+    from model import BaseModel
+    from config import CFG
+
+    # -- settings
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    DATASET_PATH = os.path.join(CFG['WORKING_DIR'], 'dataset/')
+    TEST_CSV_PATH = os.path.join(DATASET_PATH, 'test.csv')
+    SUBMIT_PATH = os.path.join(CFG['WORKING_DIR'], 'submit/')
+    
+    # -- data_loader
+    test_df = pd.read_csv(TEST_CSV_PATH)
+    test_df['path'] = test_df['path'].apply(lambda x : os.path.join(DATASET_PATH, x))
+    test_loader = load_data(test_df, test_mode=True)
+
+    # -- model
+    model = BaseModel()
+    weights_path = os.path.join('./weights', CFG['WEIGHTS'])
+    model.load_state_dict(torch.load(weights_path, map_location=device))
+
+    # -- inference
+    preds = inference(model, test_loader, device)
+
+    # -- submit
+    submit = pd.read_csv(os.path.join(DATASET_PATH, 'sample_submission.csv'))
+    submit['label'] = preds
+    submit.to_csv(os.path.join(SUBMIT_PATH, 'submit.csv'), index=False)
