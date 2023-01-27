85ff15c46734df1eb4b811ecb483c87558da4060
JeongJae Park <jeongjae1996@naver.com>
2023-01-27 22:28:22 +0900

    [feat] webcam으로 inference #9
    - f1 score 0.5의 모델을 사용하여 성능이 낮습니다.
    - 성능이 낮은 노트북 그래픽 카드 MX150을 사용하여 끊김이 있습니다.

--- examples/realtime_inference.py/realtime_inference.py
+++ examples/realtime_inference.py/realtime_inference.py
@@ -1,10 +1,67 @@
+import os
 import cv2
+import numpy as np
+import pandas as pd
 import time
+from tqdm.auto import tqdm
+
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+from torchvision import models
+
+IMG_SIZE = 160
+BATCH_SIZE = 1
+WEIGHTS_PATH = 'D:/Assignments/공모전/dacon/TVControlHandGestureRecognition/examples/baseline/template/weights/resnet3d18/best_epoch3_1.2364.pth'
+
+class CustomDataset(Dataset):
+    def __init__(self, frames, label_list):
+        self.frames = frames
+        self.label_list = label_list
+        
+    def __getitem__(self, index):
+        frames = self.frames
+        
+        if self.label_list is not None:
+            label = self.label_list[index]
+            return frames, label
+        else:
+            return frames
+        
+    def __len__(self):
+        return 1
+
+def inference(model, test_loader, device):
+    model.to(device)
+    model.eval()
+    preds = []
+    with torch.no_grad():
+        for videos in tqdm(iter(test_loader)):
+            videos = videos.to(device)
+            
+            logit = model(videos)
+
+            preds += logit.argmax(1).detach().cpu().numpy().tolist()
+    return preds
+
+# -- settings
+device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+# -- model
+model = models.video.r3d_18(weights='R3D_18_Weights.DEFAULT')
+num_features = model.fc.in_features
+model.fc = nn.Linear(num_features, 5)
+
+weights_path = WEIGHTS_PATH
+model.load_state_dict(torch.load(weights_path, map_location=device))
 
 webcam = cv2.VideoCapture(0)
 
 width = int(webcam.get(cv2.CAP_PROP_FRAME_WIDTH))
 height = int(webcam.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+# fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+# writer = cv2.VideoWriter('./output.mp4', fourcc, 30, (width, height))
 
 if not webcam.isOpened():
     print("Could not open webcam")
@@ -33,14 +90,24 @@ while webcam.isOpened():
 
     if inference_status and f <= 30:
         inference_text = f'Do a hand gesture. {f} / 30'
-        frames.append(frame)
+        img = cv2.resize(frame, (IMG_SIZE, IMG_SIZE))
+        img = img / 255.
+        frames.append(img)
         f += 1
     elif inference_status and f > 30:
         inference_status = False
         
-        
+        frames = torch.FloatTensor(np.array(frames)).permute(3, 0, 1, 2)
+        dataset = CustomDataset(frames, None)
+        dataloader = DataLoader(dataset, batch_size = BATCH_SIZE, shuffle=False, num_workers=0)
+        # -- inference
+        preds = inference(model, dataloader, device)
+        inference_text = f'inference result: {str(preds[0])}'
+
         frames = []
         f = 1
+
+    # writer.write(frame)
 
     key = cv2.waitKey(1) & 0xFF
 
@@ -51,4 +118,5 @@ while webcam.isOpened():
         break
 
 webcam.release()
+# writer.release()
 cv2.destroyAllWindows()
