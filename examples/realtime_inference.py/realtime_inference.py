fb2907004704dde3548e6d03f266c5dd3488021d
JeongJae Park <jeongjae1996@naver.com>
2023-01-27 20:09:24 +0900

    [feat] 'i'를 누르면 inference 시작하도록 구현 예정 #9
    - model inference 기능 추가 예정

--- examples/realtime_inference.py/realtime_inference.py
+++ examples/realtime_inference.py/realtime_inference.py
@@ -3,11 +3,17 @@ import time
 
 webcam = cv2.VideoCapture(0)
 
+width = int(webcam.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = int(webcam.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
 if not webcam.isOpened():
     print("Could not open webcam")
     exit()
 
 prev_time = 0
+inference_text = "press 'i' to start inference."
+inference_status = False
+f = 1
 
 while webcam.isOpened():
     status, frame = webcam.read()
@@ -20,11 +26,28 @@ while webcam.isOpened():
     text = "FPS : %0.1f" % fps
 
     cv2.putText(frame, text, (0, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0))
+    cv2.putText(frame, inference_text, (0, height - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0))
 
     if status:
         cv2.imshow("test", frame)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if inference_status and f <= 30:
+        inference_text = f'Do a hand gesture. {f} / 30'
+        frames.append(frame)
+        f += 1
+    elif inference_status and f > 30:
+        inference_status = False
+        
+        
+        frames = []
+        f = 1
+
+    key = cv2.waitKey(1) & 0xFF
+
+    if key == ord('i'):
+        inference_status = True
+        frames = []
+    elif key == ord('q'):
         break
 
 webcam.release()
